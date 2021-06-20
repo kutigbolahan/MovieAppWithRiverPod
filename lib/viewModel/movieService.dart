90bd537d470cc8e48db9bf50e5197b5f5df99a88
gbolahan <gbolahan@intelligentinnovations.co>
2021-06-20 13:52:53 +0100

    complete movie service

---
+++ lib/viewModel/movieService.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:movieWithRiverpod/model/movieModel.dart';
+import 'package:movieWithRiverpod/viewModel/environmentConfig.dart';
+
+class MovieService {
+  final EnvironmentConfig _environmentConfig;
+  final Dio _dio;
+
+  MovieService(this._environmentConfig, this._dio);
+
+  Future<List<Movie>> getMovies() async {
+    final response = await _dio.get(
+        'https://api.themoviedb.org/3/movie/popular?api_key=${_environmentConfig.movieApiKey}&languae=en-US&page=1');
+    final results = List<Map<String, dynamic>>.from(response.data['results']);
+    List<Movie> movies = results
+        .map((movieData) => Movie.fromMap(movieData))
+        .toList(growable: false);
+    return movies;
+  }
+}
+//b5f9b02d0f5d57d2f87ea214b56f63af
