04e1a7615035c5aaabad68c33bbe826588b87859
gbolahan <gbolahan@intelligentinnovations.co>
2021-06-20 13:22:42 +0100

    first riverpod provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movieWithRiverpod/homepage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
