eef929f337d19c71251b1746242709f564935315
gbolahan <gbolahan@intelligentinnovations.co>
2021-06-21 11:25:41 +0100

    movies shown

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movieWithRiverpod/model/movieModel.dart';
@@ -22,20 +24,109 @@ class MyHomePage extends ConsumerWidget {
       ),
       body: watch(moviesFutureProvider).when(
           error: (e, s) {
-            return Text('OPPS!!!');
+            return _ErrorBody(message: 'OPPS');
           },
           loading: () => Center(
                 child: CircularProgressIndicator(),
               ),
           data: (movies) {
-            return GridView.extent(
-              maxCrossAxisExtent: 200,
-              crossAxisSpacing: 12,
-              mainAxisSpacing: 12,
-              childAspectRatio: 0.7,
-              children: movies.map((movie) => Text(movie.title)).toList(),
+            return RefreshIndicator(
+              onRefresh: () {
+                return context.refresh(moviesFutureProvider);
+              },
+              child: GridView.extent(
+                maxCrossAxisExtent: 200,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 0.7,
+                children: movies
+                    .map((movie) => _MovieBox(
+                          movie: movie,
+                        ))
+                    .toList(),
+              ),
             );
           }),
+    );
+  }
+}
+
+class _ErrorBody extends StatelessWidget {
+  const _ErrorBody({
+    Key key,
+    @required this.message,
+  })  : assert(message != null, 'A non-null String must be provided'),
+        super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(message),
+          ElevatedButton(
+            onPressed: () => context.refresh(moviesFutureProvider),
+            child: Text("Try again"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MovieBox extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieBox({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          movie.fullImageUrl,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: _FrontBanner(text: movie.title),
+        ),
+      ],
+    );
+  }
+}
+
+class _FrontBanner extends StatelessWidget {
+  const _FrontBanner({
+    Key key,
+    @required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          color: Colors.grey.shade200.withOpacity(0.5),
+          height: 60,
+          child: Center(
+            child: Text(
+              text,
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
